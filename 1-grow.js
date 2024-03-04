09efd303fecd0197217cf3ba2a4f82c3f0c5ed2b
alhammanazil <alhammanazil18@gmail.com>
2024-03-04 16:32:31 +0700

    update description for 1/366

--- 1-grow.js
+++ 1-grow.js
@@ -1,3 +1,7 @@
+// WPU Coding Challenge 2024
+// 1/366
+// https://www.codewars.com/kata/5513795bd3fafb56c200049e
+
 // function grow(x) {
 //   let result = x[0];
 //   for (let i = 1; i < x.length; i++) {
