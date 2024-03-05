bb4f783ea08b9b04aa9cf9ef5320ef66705cd316
alhammanazil <alhammanazil18@gmail.com>
2024-03-05 09:47:14 +0700

    5/366

---
+++ 5-zeroFuel.js
@@ -0,0 +1,12 @@
+// WPU Coding Challenge 2024
+// 5/366
+// https://www.codewars.com/kata/5861d28f124b35723e00005e
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//     return fuelLeft * mpg >= distanceToPump;
+//   };
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {return fuelLeft * mpg >= distanceToPump;}
+
+// Test cases
+console.log(zeroFuel(100, 50, 2));
