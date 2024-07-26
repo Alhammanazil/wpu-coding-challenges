356297fe1048e41578306cb9a61c464066c63b15
Alhammanazil <alhammanazil18@gmail.com>
2024-07-26 10:17:54 +0700

    26/366

---
+++ 26-updateLight.js
@@ -0,0 +1,23 @@
+// WPU Coding Challenge 2024
+// 25/366
+// https://www.codewars.com/kata/58649884a1659ed6cb000072
+
+// menggunakan switch
+function updateLight(current) {
+    switch (current) {
+        case 'green':
+            return 'yellow';
+        case 'yellow':
+            return 'red';
+        case 'red':
+            return 'green';
+    }
+}
+
+// menggunakan ternary
+const updateLight = current => current === 'green' ? 'yellow' : current === 'yellow' ? 'red' : 'green';
+
+// Test cases
+console.log(updateLight('green'))
+console.log(updateLight('yellow'))
+console.log(updateLight('red'))
