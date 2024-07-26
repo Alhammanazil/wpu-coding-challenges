1c97c176781bf38ae65072bca87b4de9b9d405ee
Alhammanazil <alhammanazil18@gmail.com>
2024-07-26 09:19:40 +0700

    22/366

---
+++ 22-removeExclamationMarks.js
@@ -0,0 +1,12 @@
+// WPU Coding Challenge 2024
+// 21/366
+// https://www.codewars.com/kata/57a0885cbb9944e24c00008e
+
+// function removeExclamationMarks(s) {
+//     return s.split('!').join('')
+//   }
+
+const removeExclamationMarks = s => s.split('!').join('')
+
+  // Test cases
+  console.log(removeExclamationMarks("Hello World!"))
