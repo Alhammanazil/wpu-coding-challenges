2fd8e965f4462b3a337d54c23950bd0f59b66812
Alhammanazil <alhammanazil18@gmail.com>
2024-11-18 10:46:19 +0700

    32/366

---
+++ 32-cockroachSpeed.js
@@ -0,0 +1,13 @@
+// WPU Coding Challenge 2024
+// 32/366
+// https://www.codewars.com/kata/5a4e378c555727a84c00005e
+
+// function cockroachSpeed(s) {
+//     return Math.floor(s * 100000 / 3600);
+// }
+
+// arrow function
+const cockroachSpeed = s => Math.floor(s * 100000 / 3600);
+
+// Test cases
+console.log(cockroachSpeed(1.08));
